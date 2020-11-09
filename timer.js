8af4f28f13f931e6c1d21db30aa3fd45f8b3d3f8
Erickson Guffey <eguffeyarmy2@gmail.com>
2020-11-09 12:38:28 -0500

    timer needs PHP, works

---
+++ timer.js
@@ -0,0 +1,31 @@
+var restEst = 045; //this value will change depending on restaurant input
+
+document.getElementById('timer').innerHTML = restEst + ":" + 01;
+
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if (s == 59) {
+    m = m - 1;
+  }
+  if (m < 0) {
+    document.getElementById('timer').innerHTML = "Food is Ready";
+  }
+  document.getElementById('timer').innerHTML = m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {
+    sec = "0" + sec
+  };
+  if (sec < 0) {
+    sec = "59"
+  };
+  return sec;
+}
